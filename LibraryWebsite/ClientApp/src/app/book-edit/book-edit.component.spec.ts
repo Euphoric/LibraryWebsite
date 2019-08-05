05a3d0b8e05d01ae23c15aabc77ad43be1182172
Radek Falhar <Radek.Falhar@hotmail.com>
2019-08-05 15:49:09 +0200

    Fixed book-list and book-edit tests.

--- LibraryWebsite/ClientApp/src/app/book-edit/book-edit.component.spec.ts
+++ LibraryWebsite/ClientApp/src/app/book-edit/book-edit.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 import { BookEditComponent } from './book-edit.component';
 
@@ -8,6 +11,8 @@ describe('BookEditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, FormsModule, HttpClientTestingModule],
+      providers: [{ provide: 'BASE_URL', useValue: 'www.test.com', },],
       declarations: [ BookEditComponent ]
     })
     .compileComponents();
--- LibraryWebsite/ClientApp/src/app/book-list/book-list.component.spec.ts
+++ LibraryWebsite/ClientApp/src/app/book-list/book-list.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { BookListComponent } from './book-list.component';
 
@@ -8,7 +10,9 @@ describe('BookListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookListComponent ]
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [{ provide: 'BASE_URL', useValue: 'www.test.com', },],
+      declarations: [ BookListComponent ] 
     })
     .compileComponents();
   }));
