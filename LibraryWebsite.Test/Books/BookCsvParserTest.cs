2164ebb31d6c78566708a24042ed607bb6f39b76
Radek Falhar <Radek.Falhar@hotmail.com>
2019-09-02 21:03:22 +0200

    Implemented parsing of books from csv file.

---
+++ LibraryWebsite.Test/Books/BookCsvParserTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace LibraryWebsite.Books
+{
+    public class BookCsvParserTest
+    {
+        [Fact]
+        public void Parse_null_is_error()
+        {
+            var parser = new BookCsvParser();
+            Assert.Throws<ArgumentNullException>(() => parser.Parse(null));
+        }
+
+        [Fact]
+        public void Parse_nonexistent_file_is_error()
+        {
+            var parser = new BookCsvParser();
+            Assert.Throws<FileNotFoundException>(() => parser.Parse("no_file.csv"));
+        }
+
+        [Fact]
+        public void Empty_file_is_error()
+        {
+            var parser = new BookCsvParser();
+            Assert.Throws<Exception>(() => parser.Parse("Books/TestData/empty_file.csv"));
+        }
+
+        [Theory]
+        [InlineData(1, 100)]
+        [InlineData(2, 21)]
+        public void Parses_file_lines(int datasetId, int expectedCount)
+        {
+            var parser = new BookCsvParser();
+            var books = parser.Parse($"Books/TestData/books-small{datasetId}.csv");
+            Assert.NotNull(books);
+            Assert.Equal(expectedCount, books.Count());
+
+            var firstBook = books.First();
+
+            BookCsvParser.ParsedBook expectedFirstBook = 
+                datasetId == 1 ?
+                new BookCsvParser.ParsedBook() { Id = 1, Title = "The Hunger Games", Authors = "Suzanne Collins", Isbn13 = "9780439023480" } :
+                new BookCsvParser.ParsedBook() { Id = 110, Title = "A Clash of Kings", Authors = "George R.R. Martin", Isbn13 = "9780553381700" }
+                ;
+
+            Assert.Equal(expectedFirstBook.Id, firstBook.Id);
+            Assert.Equal(expectedFirstBook.Title, firstBook.Title);
+            Assert.Equal(expectedFirstBook.Authors, firstBook.Authors);
+            Assert.Equal(expectedFirstBook.Isbn13, firstBook.Isbn13);
+        }
+    }
+}
---
+++ LibraryWebsite/Books/BookCsvParser.cs
@@ -0,0 +1,72 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using CsvHelper.TypeConversion;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryWebsite.Books
+{
+    public class BookCsvParser
+    {
+        public class ParsedBook
+        {
+            [Name("book_id")]
+            public int Id { get; set; }
+
+            [Name("original_title")]
+            public string Title { get; set; }
+
+            [Name("authors")]
+            public string Authors { get; set; }
+
+            [Name("isbn13")]
+            [TypeConverter(typeof(Isbn13Converter))]
+            public string Isbn13 { get; set; }
+        }
+
+        private class Isbn13Converter : DefaultTypeConverter
+        {
+            public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+            {
+                if (text == string.Empty)
+                    return string.Empty;
+
+                var val = double.Parse(text, CultureInfo.InvariantCulture);
+                return val.ToString("G13", CultureInfo.InvariantCulture);
+            }
+
+            public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public IEnumerable<ParsedBook> Parse(string booksFilename)
+        {
+            if (booksFilename == null)
+                throw new ArgumentNullException(nameof(booksFilename));
+
+            if (!File.Exists(booksFilename))
+            {
+                throw new FileNotFoundException("File not found!", booksFilename);
+            }
+
+            if (string.IsNullOrWhiteSpace(File.ReadAllText(booksFilename)))
+            {
+                throw new Exception("File didnd't contain valid data.");
+            }
+
+            using (var reader = new StreamReader(booksFilename))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Configuration.Delimiter = ",";
+                return csv.GetRecords<ParsedBook>().ToArray();
+            }
+        }
+    }
+}
