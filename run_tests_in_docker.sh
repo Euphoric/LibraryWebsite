9b6949adf169807744930f87fb1a96c63b7e3ae1
Radek Falhar <Radek.Falhar@hotmail.com>
2019-09-22 12:35:53 +0200

    Removed unecessary node code from test dockerfile.

--- run_tests_in_docker.sh
+++ run_tests_in_docker.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # Exit if any of the commands fail
-set -e 
+set -e
 
-case $1 in
+case "$1" in
 
-  'database')
+  "database")
     IntegrationTest/wait_for_it.sh $DATABASE_SERVER_WAIT -t 30
     dotnet test LibraryWebsite.TestDatabase -c Release --no-build --logger "trx;LogFileName=../../TestResults/DatabaseTestResult.trx"
     ;;
 
-  'endToEnd')
+  "endToEnd")
     IntegrationTest/wait_for_it.sh $WEB_ADDRESS -t 30
     dotnet test LibraryWebsite.TestEndToEnd -c Release --no-build --logger "trx;LogFileName=../../TestResults/EndToEndTestResult.trx"
     ;;
 
-  'unit_dotnet')
+  "unit_dotnet")
     dotnet test LibraryWebsite.Test -c Release --no-build --logger "trx;LogFileName=../../TestResults/UnitTestResult.trx"
     ;;
 	
