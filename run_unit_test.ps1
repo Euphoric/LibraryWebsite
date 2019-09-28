9c2d8ffb9904ff72ce50e537d4a6fcaad190ccee
Radek Falhar <Radek.Falhar@hotmail.com>
2019-09-28 09:40:45 +0200

    Changed Angular Unit tests to use docker.

--- run_unit_test.ps1
+++ run_unit_test.ps1
@@ -1,2 +1,5 @@
 docker build -t radekfalhar/librarywebsite_test:latest -f Dockerfile-test .
+docker build -t radekfalhar/librarywebsite_test_angular:latest -f Dockerfile-test-angular .
+
 docker run -v TestResults:/src/TestResults radekfalhar/librarywebsite_test unit_dotnet
+docker run -v TestResults:/src/TestResults radekfalhar/librarywebsite_test_angular
