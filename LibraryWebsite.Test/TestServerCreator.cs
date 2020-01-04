20bbb537e53ace4fef5d8df89a922dc10dbde525
Radek Falhar <Radek.Falhar@hotmail.com>
2020-01-04 20:43:09 +0100

    Changed TestServer creation to have "Testing" environment, and load configuration from file.

--- LibraryWebsite.Test/TestServerCreator.cs
+++ LibraryWebsite.Test/TestServerCreator.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Xunit.Abstractions;
 
@@ -14,27 +12,11 @@ namespace LibraryWebsite
     {
         public static TestServer CreateTestServer(ITestOutputHelper outputHelper)
         {
-            var config = new ConfigurationBuilder()
-                .AddInMemoryCollection(new Dictionary<string, string>
-                {
-                    {"Logging:LogLevel:Default", "Warning"},
-                    {"Security:Secret", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
-                })
-                .Build();
-
             var builder = WebHost.CreateDefaultBuilder()
-                .UseConfiguration(config)
-                .UseEnvironment(Environments.Production)
+                .UseEnvironment("Testing")
                 .UseStartup<Startup>()
                 .ConfigureTestServices(TestingStartup.ConfigureServices)
-
-                // override default startup configuration for testing purposes
-                .ConfigureLogging(logging =>
-                    logging
-                        .ClearProviders()
-                        .AddConfiguration(config.GetSection("Logging"))
-                        .AddXUnit(outputHelper));
-                ;
+                .ConfigureLogging(logging => logging.AddXUnit(outputHelper));
 
             var testServer = new TestServer(builder);
             testServer.BaseAddress = new Uri("https://localhost/"); // use HTTPS for all requests
