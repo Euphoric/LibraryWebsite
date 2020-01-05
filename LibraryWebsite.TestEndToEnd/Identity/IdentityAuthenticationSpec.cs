1577dfc7973cabe2a560115659fa74422be7d2fd
Radek Falhar <Radek.Falhar@hotmail.com>
2020-01-05 15:50:29 +0100

    Fixed Identity authentication tests to work with https.

--- LibraryWebsite.TestEndToEnd/Identity/IdentityAuthenticationSpec.cs
+++ LibraryWebsite.TestEndToEnd/Identity/IdentityAuthenticationSpec.cs
@@ -1,25 +1,23 @@
 ﻿using System;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using IdentityModel.Client;
-using LibraryWebsite.TestEndToEnd;
 using Xunit;
-using Xunit.Abstractions;
 
-namespace LibraryWebsite.Identity
+namespace LibraryWebsite.TestEndToEnd.Identity
 {
-    public class IdentityAuthenticationApiTest
+    public class IdentityAuthenticationSpec
     {
-        public IdentityAuthenticationApiTest(ITestOutputHelper outputHelper)
-        {
-            using HttpClient client = new HttpClient();
-        }
-
         [Fact]
         public async Task Discovery_document_retrieved()
         {
-            using HttpClient client = new HttpClient {BaseAddress = new Uri(WebAddresses.WebsiteUri)};
+            using var httpClientHandler = new HttpClientHandler
+            {
+                // ignores certificate errors
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
+            };
+
+            using var client = new HttpClient(httpClientHandler) {BaseAddress = new Uri(WebAddresses.WebsiteUri)};
 
             var discoveryDocumentResponse = await client.GetDiscoveryDocumentAsync();
             Assert.False(discoveryDocumentResponse.IsError, discoveryDocumentResponse.Error);
@@ -28,7 +26,13 @@ namespace LibraryWebsite.Identity
         [Fact]
         public async Task Client_authenticated()
         {
-            using HttpClient client = new HttpClient {BaseAddress = new Uri(WebAddresses.WebsiteUri)};
+            using var httpClientHandler = new HttpClientHandler
+            {
+                // ignores certificate errors
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
+            };
+
+            using var client = new HttpClient(httpClientHandler) {BaseAddress = new Uri(WebAddresses.WebsiteUri)};
 
             var discoveryDocument = await client.GetDiscoveryDocumentAsync();
             Assert.False(discoveryDocument.IsError);
