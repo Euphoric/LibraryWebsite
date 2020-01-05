482406a67d3fde9980c49dfa1a8876d304dcf52e
Radek Falhar <Radek.Falhar@hotmail.com>
2020-01-05 10:20:37 +0100

    Added identity end to end tests.

---
+++ LibraryWebsite.TestEndToEnd/Identity/IdentityAuthenticationApiTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using LibraryWebsite.TestEndToEnd;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LibraryWebsite.Identity
+{
+    public class IdentityAuthenticationApiTest
+    {
+        public IdentityAuthenticationApiTest(ITestOutputHelper outputHelper)
+        {
+            using HttpClient client = new HttpClient();
+        }
+
+        [Fact]
+        public async Task Discovery_document_retrieved()
+        {
+            using HttpClient client = new HttpClient {BaseAddress = new Uri(WebAddresses.WebsiteUri)};
+
+            var discoveryDocumentResponse = await client.GetDiscoveryDocumentAsync();
+            Assert.False(discoveryDocumentResponse.IsError, discoveryDocumentResponse.Error);
+        }
+
+        [Fact]
+        public async Task Client_authenticated()
+        {
+            using HttpClient client = new HttpClient {BaseAddress = new Uri(WebAddresses.WebsiteUri)};
+
+            var discoveryDocument = await client.GetDiscoveryDocumentAsync();
+            Assert.False(discoveryDocument.IsError);
+
+            var response = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+            {
+                Address = discoveryDocument.TokenEndpoint,
+                ClientId = "PublicApi"
+            });
+
+            Assert.False(response.IsError, response.Error);
+            Assert.NotNull(response.AccessToken);
+        }
+    }
+}
