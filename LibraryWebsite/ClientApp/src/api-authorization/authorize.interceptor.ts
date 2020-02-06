2c118f577c43cd90045abb3a5806f6b56bd48273
Radek Falhar <Radek.Falhar@hotmail.com>
2020-02-06 21:08:26 +0100

    Fixed recursive HTTP interception of authentication configuration request.

--- LibraryWebsite/ClientApp/src/api-authorization/authorize.interceptor.ts
+++ LibraryWebsite/ClientApp/src/api-authorization/authorize.interceptor.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { AuthorizeService } from './authorize.service';
 import { mergeMap } from 'rxjs/operators';
+import { ApplicationPaths } from './api-authorization.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class AuthorizeInterceptor implements HttpInterceptor {
   constructor(private authorize: AuthorizeService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.includes(ApplicationPaths.ApiAuthorizationClientConfigurationUrl))
+    {
+      return from(next.handle(req));
+    }
     return this.authorize.getAccessToken()
       .pipe(mergeMap(token => this.processRequestWithToken(token, req, next)));
   }
