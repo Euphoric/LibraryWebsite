9a153b1b3496122996ed3518d57069b187f8bacf
Radek Falhar <Radek.Falhar@hotmail.com>
2020-02-16 18:30:50 +0100

    Adding 15s sleep after database first responsds in Database and E2E tests. This might fix random failures in CI.

--- run_tests_in_docker.sh
+++ run_tests_in_docker.sh
@@ -7,11 +7,15 @@ case "$1" in
 
   "database")
     sh ./IntegrationTest/wait_for_it.sh $DATABASE_SERVER_WAIT -t 30
+	# the database might take few seconds to get setup even after it starts accepting requests
+	sleep 15s
     dotnet test LibraryWebsite.TestDatabase -c Release --no-build --logger "trx;LogFileName=../../TestResults/DatabaseTestResult.trx"
     ;;
 
   "endToEnd")
     sh ./IntegrationTest/wait_for_it.sh $WEB_ADDRESS -t 30
+	# the database might take few seconds to get setup even after it starts accepting requests
+	sleep 15s
     dotnet test LibraryWebsite.TestEndToEnd -c Release --no-build --logger "trx;LogFileName=../../TestResults/EndToEndTestResult.trx"
     ;;
 
