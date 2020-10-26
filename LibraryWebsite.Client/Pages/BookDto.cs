712204ed07642ffb08b2dc21b462833b33612d02
Radek Falhar <Radek.Falhar@hotmail.com>
2020-10-26 20:03:35 +0100

    Add book edit form.
    Not working due to missing authorization.

---
+++ LibraryWebsite.Client/Pages/BookDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryWebsite.Client.Pages
+{
+    public class BookDto
+    {
+        public Guid Id { get; set; }
+
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Description { get; set; }
+
+        public string? Isbn13 { get; set; }
+    }
+}
