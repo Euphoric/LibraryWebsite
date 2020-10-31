59e6473834881881bf9e22bd64982ec9b6826add
Radek Falhar <Radek.Falhar@hotmail.com>
2020-10-31 19:02:20 +0100

    Move client book pages into separate folder.

--- LibraryWebsite.Client/Books/BookDto.cs
+++ LibraryWebsite.Client/Books/BookDto.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LibraryWebsite.Pages
+namespace LibraryWebsite.Books
 {
     public class BookDto
     {
