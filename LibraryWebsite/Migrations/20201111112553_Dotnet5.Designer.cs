e7886af61b2e90d7bb79a770ec5b643675e00d3d
Radek Falhar <Radek.Falhar@hotmail.com>
2020-11-11 12:29:55 +0100

    Migrate database to new structure from .NET 5
    Fixes login.

--- LibraryWebsite/Migrations/20201111112553_Dotnet5.Designer.cs
+++ LibraryWebsite/Migrations/20201111112553_Dotnet5.Designer.cs
@@ -4,14 +4,16 @@ using LibraryWebsite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LibraryWebsite.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20201111112553_Dotnet5")]
+    partial class Dotnet5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LibraryWebsite/Migrations/20201111112553_Dotnet5.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryWebsite.Migrations
+{
+    public partial class Dotnet5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "41ef98ad-2338-4cd8-b154-dcbaf21ce424");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6e29740f-407f-4f3e-b4a4-744344d0f2a7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7e636afb-8e97-4619-a0d6-bf564442f09a");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ConsumedTime",
+                table: "PersistedGrants",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "PersistedGrants",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SessionId",
+                table: "PersistedGrants",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "DeviceCodes",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SessionId",
+                table: "DeviceCodes",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f6c5d2e0-86e4-478d-b951-7e3ea96000d5", "c4535370-811e-4497-aa2b-ef63f908de64", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b4693c03-eabc-4193-8046-e436bfd80fc1", "f12010e8-b2bb-4af5-add0-69fa873731af", "Librarian", "LIBRARIAN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "08c55fb2-5ed3-4a46-8126-d820879e61c2", "022e793d-1c35-4a7c-a712-1f0ba95115dd", "Reader", "READER" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersistedGrants_SubjectId_SessionId_Type",
+                table: "PersistedGrants",
+                columns: new[] { "SubjectId", "SessionId", "Type" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PersistedGrants_SubjectId_SessionId_Type",
+                table: "PersistedGrants");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "08c55fb2-5ed3-4a46-8126-d820879e61c2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4693c03-eabc-4193-8046-e436bfd80fc1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f6c5d2e0-86e4-478d-b951-7e3ea96000d5");
+
+            migrationBuilder.DropColumn(
+                name: "ConsumedTime",
+                table: "PersistedGrants");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "PersistedGrants");
+
+            migrationBuilder.DropColumn(
+                name: "SessionId",
+                table: "PersistedGrants");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "DeviceCodes");
+
+            migrationBuilder.DropColumn(
+                name: "SessionId",
+                table: "DeviceCodes");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "6e29740f-407f-4f3e-b4a4-744344d0f2a7", "bda3cb1e-6ecc-4906-8f86-edd1faa96a4a", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "41ef98ad-2338-4cd8-b154-dcbaf21ce424", "70e220a7-6899-4b98-8153-f8ba8766a542", "Librarian", "LIBRARIAN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "7e636afb-8e97-4619-a0d6-bf564442f09a", "d7b2db1a-158d-4565-91f9-6231319787ce", "Reader", "READER" });
+        }
+    }
+}
