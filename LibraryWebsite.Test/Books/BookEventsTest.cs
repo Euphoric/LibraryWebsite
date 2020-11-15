3f509058d17742015d206cfe84b881d2c0997ee9
Radek Falhar <Radek.Falhar@hotmail.com>
2020-11-15 20:43:10 +0100

    Implement book aggregate.

---
+++ LibraryWebsite.Test/Books/BookEventsTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Euphoric.EventModel;
+using NodaTime;
+using Xunit;
+
+namespace LibraryWebsite.Books
+{
+    public class BookEventsTest
+    {
+        private readonly IEventStore _eventStore;
+
+        public BookEventsTest()
+        {
+            var clock = new NodaTime.Testing.FakeClock(Instant.FromUtc(2020, 01, 01, 01, 01, 01));
+            var domainEventFactory = new DomainEventFactory(new EventTypeLocator(typeof(BookDomainEvent).Assembly));
+            _eventStore = new InMemoryEventStore(new DomainEventSender(new List<IDomainEventListener>()), domainEventFactory, clock);       
+        }
+
+        [Fact]
+        public async Task Creates_book_aggregate()
+        {
+            var evnt = BookAggregate.New("Title X", "Author X", "ISBN13 X", "Description X");
+            var storedEvent = await _eventStore.Store(evnt);
+            var aggregate = AggregateBuilder<BookAggregate>.Rehydrate(new [] { storedEvent });
+
+            Assert.Equal("Title X", aggregate.Title);
+            Assert.Equal("Author X", aggregate.Author);
+            Assert.Equal("ISBN13 X", aggregate.Isbn13);
+            Assert.Equal("Description X", aggregate.Description);
+        }
+
+        [Fact]
+        public async Task Changes_book_aggregate()
+        {
+            var evnt = BookAggregate.New("Title X", "Author X", "ISBN13 X", "Description X");
+            var storedEvent = await _eventStore.Store(evnt);
+            var aggregate = AggregateBuilder<BookAggregate>.Rehydrate(new [] { storedEvent });
+
+            var evnt2 = aggregate.Change("Title Y", "Author Y", "ISBN13 Y", "Description Y");
+            var storedEvent2 = await _eventStore.Store(evnt2);
+            var aggregate2 = AggregateBuilder<BookAggregate>.Update(aggregate, new [] { storedEvent2 });
+
+            Assert.Equal("Title Y", aggregate2.Title);
+            Assert.Equal("Author Y", aggregate2.Author);
+            Assert.Equal("ISBN13 Y", aggregate2.Isbn13);
+            Assert.Equal("Description Y", aggregate2.Description);
+        }
+
+        [Fact]
+        public async Task Deletes_book_aggregate()
+        {
+            var evnt = BookAggregate.New("Title X", "Author X", "ISBN13 X", "Description X");
+            var storedEvent = await _eventStore.Store(evnt);
+            var aggregate = AggregateBuilder<BookAggregate>.Rehydrate(new [] { storedEvent });
+
+            Assert.False(aggregate.IsDeleted);
+
+            var evnt2 = aggregate.Delete();
+            var storedEvent2 = await _eventStore.Store(evnt2);
+            var aggregate2 = AggregateBuilder<BookAggregate>.Update(aggregate, new [] { storedEvent2 });
+
+            Assert.True(aggregate2.IsDeleted);
+        }
+
+        [Fact]
+        public async Task Cannot_change_when_deleted()
+        {
+            var evnt = BookAggregate.New("Title X", "Author X", "ISBN13 X", "Description X");
+            var storedEvent = await _eventStore.Store(evnt);
+            var aggregate = AggregateBuilder<BookAggregate>.Rehydrate(new [] { storedEvent });
+
+            Assert.False(aggregate.IsDeleted);
+
+            var evnt2 = aggregate.Delete();
+            var storedEvent2 = await _eventStore.Store(evnt2);
+            var aggregate2 = AggregateBuilder<BookAggregate>.Update(aggregate, new [] { storedEvent2 });
+
+            Assert.Throws<AggregateDeletedException>(()=>aggregate2.Change("Title Y", "Author Y", "ISBN13 Y", "Description Y"));
+        }
+    }
+}
---
+++ LibraryWebsite/Books/BookEvents.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Euphoric.EventModel;
+
+namespace LibraryWebsite.Books
+{
+    public record BookKey(Guid id) : IAggregateKey<BookAggregate, BookDomainEvent>
+    {
+        public static PrefixedGuidKey<BookKey> Format { get; } = new PrefixedGuidKey<BookKey>("book");
+
+        public string Value => Format.ToValue(id);
+
+        public static implicit operator EntityId(BookKey key) => new EntityId(key.Value);
+        public static implicit operator BookKey(EntityId id) => Format.Parse(id.Value);
+
+        public static EntityId ToEntityId(BookKey key) => new EntityId(key.Value);
+        public static BookKey FromEntityId(EntityId id) => Format.Parse(id.Value);
+    }
+
+    public record BookAggregate(BookKey Id, string Title, string Author, string Isbn13, string Description, bool IsDeleted) : Aggregate
+    {
+        #region Rehydrate
+
+        public static BookAggregate Initialize(IDomainEvent<BookCreated> createdEvent)
+        {
+            var eventData = createdEvent.Data;
+            return new BookAggregate(eventData.Id, eventData.Title, eventData.Author, eventData.Isbn13, eventData.Description, false);
+        }
+
+        public BookAggregate Update(IDomainEvent<BookDomainEvent> evnt)
+        {
+            switch (evnt.Data)
+            {
+                case BookDeleted:
+                    return this with { IsDeleted = true };
+                case BookChanged eventData:
+                    return this with
+                    {
+                        Title = eventData.Title,
+                        Author = eventData.Author,
+                        Isbn13 =
+                        eventData.Isbn13,
+                        Description = eventData.Description
+                    };
+                default:
+                    throw new NotSupportedException("Unknown event type.");
+            }
+        }
+
+        #endregion
+
+        #region Modify
+
+        public static ICreateEvent<BookDomainEvent> New(string title, string author, string isbn13, string description)
+        {
+            BookKey newId = BookKey.Format.New();
+            return new BookCreated(newId, title, author, isbn13, description).AsNewAggregate();
+        }
+
+        public ICreateEvent<BookDomainEvent> Change(string title, string author, string isbn13, string description)
+        {
+            if (IsDeleted)
+            {
+                throw new AggregateDeletedException($"Book {Id} is deleted.");
+            }
+
+            return new BookChanged(Id, title, author, isbn13, description).AsNewAggregate();
+        }
+
+        public ICreateEvent<BookDomainEvent> Delete()
+        {
+            return new BookDeleted(Id).AsVersioned(Version);
+        }
+
+        #endregion
+    }
+
+    public record BooksListProjection : IProjection
+    {
+        ImmutableDictionary<BookKey, BookDto> TodoItems { get; init; } = ImmutableDictionary<BookKey, BookDto>.Empty;
+
+        public IEnumerable<BookDto> TodoList()
+        {
+            return TodoItems.Values;
+        }
+
+        public IProjection NextState(IDomainEvent<IDomainEventData> evnt)
+        {
+            switch (evnt.Data)
+            {
+                case BookCreated created:
+                    var newItem = new BookDto() { Id = created.Id, Title = created.Title };
+                    return this with { TodoItems = TodoItems.Add(created.Id, newItem) };
+                case BookDeleted deleted:
+                    return this with { TodoItems = TodoItems.Remove(deleted.Id) };
+                default:
+                    return this;
+            }
+        }
+    }
+
+    public abstract record BookDomainEvent(BookKey Id) : IDomainEventData
+    {
+        public string GetAggregateKey()
+        {
+            return Id.Value;
+        }
+    }
+
+    [DomainEvent("book-created")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Record bug")]
+    public record BookCreated(BookKey Id, string Title, string Author, string Isbn13, string Description) : BookDomainEvent(Id)
+    {
+    }
+
+    [DomainEvent("book-changed")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Record bug")]
+    public record BookChanged(BookKey Id, string Title, string Author, string Isbn13, string Description) : BookDomainEvent(Id)
+    {
+    }
+
+    [DomainEvent("book-deleted")]
+    public record BookDeleted(BookKey Id) : BookDomainEvent(Id)
+    {
+    }
+
+    public class AggregateDeletedException : Exception
+    {
+        public AggregateDeletedException()
+        {
+        }
+
+        public AggregateDeletedException(string message) : base(message)
+        {
+        }
+
+        public AggregateDeletedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
