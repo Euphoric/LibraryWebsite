a262aa65ba87766177dd5283eee8027670e8ba88
Radek Falhar <Radek.Falhar@hotmail.com>
2020-12-05 12:56:33 +0100

    Run end to end tests with EventStore

--- run_endToEnd_test.ps1
+++ run_endToEnd_test.ps1
@@ -1,2 +1,3 @@
 docker build -t radekfalhar/librarywebsite:latest -f LibraryWebsite/Dockerfile .
-docker-compose -f docker-compose.endtoendtest.yml up --build --abort-on-container-exit --exit-code-from end_to_end_test
+docker-compose -f docker-compose.setup_cert.yml up
+docker-compose -f docker-compose.endtoendtest.yml up --abort-on-container-exit --exit-code-from end_to_end_test
