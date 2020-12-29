dbab58abe4a2384604a254fd0684216c7290216b
Radek Falhar <Radek.Falhar@hotmail.com>
2020-12-29 16:29:51 +0100

    Remove entity framework LibraryContext
    As it is unused now

--- LibraryWebsite.Test/TestingStartup.cs
+++ LibraryWebsite.Test/TestingStartup.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using System;
 using Euphoric.EventModel;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using NodaTime;
 using NodaTime.Testing;
 
@@ -12,10 +10,6 @@ namespace LibraryWebsite
     {
         public static void ConfigureServices(IServiceCollection services)
         {
-            services.RemoveAll<DbContextOptions<LibraryContext>>();
-            var dbName = Guid.NewGuid().ToString();
-            services.AddDbContext<LibraryContext>(options => options.UseInMemoryDatabase(dbName));
-
             services.AddSingleton<IEventStore, InMemoryEventStore>();
             services.AddSingleton<IProjectionContainerFactory, SynchronousProjectionContainerFactory>();
 
